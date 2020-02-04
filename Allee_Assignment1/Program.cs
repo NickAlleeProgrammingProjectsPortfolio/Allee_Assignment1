946976151c07b8516ca290968a4b38e30d5d06da
Nicholas Allee <42816286+jiggensnaggen@users.noreply.github.com>
2020-02-04 16:00:43 -0600

    Add project files.

---
+++ Allee_Assignment1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Allee_Assignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int stopper = 0;
+            while (stopper == 0)
+            {
+                Console.WriteLine("**** Welcome to B&B Theatre ****");
+
+                Console.WriteLine("Please enter the full name:");
+                var name = Console.ReadLine();
+
+                while (string.IsNullOrEmpty(name))
+                {
+                    Console.WriteLine("Sorry, Please enter your name?");
+                    name = Console.ReadLine();
+                }
+
+
+                Console.WriteLine("Please enter your age?");
+                var age = Console.ReadLine();
+
+                while (string.IsNullOrEmpty(name))
+                {
+                    Console.WriteLine("Sorry, Please enter your age?");
+                    name = Console.ReadLine();
+                }
+                int intAge;
+                while (int.TryParse(age, out intAge))
+                {
+                    if (intAge < 15)
+                    {
+                        Console.WriteLine("Your Age is below 15, you cannot book ticket online. Please talk to your parent or guardian!!");
+                        stopper = 1;
+                    }
+                    if (intAge > 90)
+                    {
+                        Console.WriteLine("You are too old to watch a movie, sorry!");
+                        stopper = 1;
+                    }
+                }
+            }
+
+        }
+    }
+}
